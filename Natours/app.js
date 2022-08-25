bddfed8f42f817b11680443d86c44bc9c1b56f06
Babarali345 <babar@tecizeverything.com>
2022-08-25 16:39:26 +0500

    Creating and Mounting Multiple Routers

--- Natours/app.js
+++ Natours/app.js
@@ -1,24 +1,29 @@
 const fs = require("fs");
 const express = require("express");
+const morgan = require("morgan");
+const { createSecurePair } = require("tls");
 const app = express();
+//1 MIDDELE_WARE
 app.use(express.json());
+app.use(morgan("dev"));
 app.use((req, res, next) => {
   console.log("Hellow from middleware ");
   next();
 });
 app.use((req, res, next) => {
-  req.requestTime=new Date().toISOString()
+  req.requestTime = new Date().toISOString();
   next();
 });
 const tours = JSON.parse(
   fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
 );
 
+//2 METHOOD
 const getAllTourse = (req, res) => {
-  console.log(req.requestTime)
+  console.log(req.requestTime);
   res.status(200).json({
     status: "Success",
-    requestedAt:req.requestTime,
+    requestedAt: req.requestTime,
     result: tours.length,
     data: {
       tours,
@@ -96,31 +101,55 @@ const deleteTour = (req, res) => {
   });
 };
 
-app.route("/api/v1/tours").get(getAllTourse).post(createTour);
+const getAllUsers = (req, res) => {
+  res.status(500).json({
+    status: "error",
+    message: "This route is not yet define",
+  });
+};
+const getUser = (req, res) => {
+  res.status(500).json({
+    status: "error",
+    message: "This route is not yet define",
+  });
+};
+const createUser = (req, res) => {
+  res.status(500).json({
+    status: "error",
+    message: "This route is not yet define",
+  });
+};
+const updateUser = (req, res) => {
+  res.status(500).json({
+    status: "error",
+    message: "This route is not yet define",
+  });
+};
+const deleteUser = (req, res) => {
+  res.status(500).json({
+    status: "error",
+    message: "This route is not yet define",
+  });
+};
+//3 ROUTS
+const tourRouter=express.Router()
+const userRouter=express.Router()
+tourRouter.route("/").get(getAllTourse).post(createTour);
 
-// If we use Midleware here and consumee getAllTours APi
-//then middle ware doesn't called because api Request responces finshed
-// using middleware order Really maters
-
-{
-  /* *****************************************/
-}
-
-// app.use((req,res,next)=>{
-//   console.log('Hellow from middleware ');
-//   next()
-// })
-
-{
-  /* *****************************************/
-}
-app
-  .route("/api/v1/tours/:id")
+tourRouter
+  .route("/:id")
   .get(getTour)
   .patch(updateTour)
   .delete(deleteTour);
+
+userRouter.route("/").get(getAllUsers).post(createUser);
+userRouter.route("/:id").get(getUser).patch(updateUser).delete(deleteUser);
+
+app.use("/api/v1/tours",tourRouter)
+app.use("/api/v1/user",userRouter)
 const port = 300;
 
+//4 SERVER
 app.listen(port, () => {
   console.log("appp runing");
 });
