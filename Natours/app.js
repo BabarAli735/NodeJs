aa5d145560512c1cd4fd8b83af5604a664cc1b30
Babarali345 <babar@tecizeverything.com>
2022-08-26 16:37:29 +0500

    Complete Setion 6

--- Natours/app.js
+++ Natours/app.js
@@ -1,25 +1,18 @@
-
 const express = require("express");
 const morgan = require("morgan");
 const app = express();
 //1 MIDDELE_WARE
 app.use(express.json());
-app.use(morgan("dev"));
-app.use(express.static(`${__dirname}/public`))
+if (process.env.NODE_ENV === "development") {
+  app.use(morgan("dev"));
+}
+app.use(express.static(`${__dirname}/public`));
 
 //2 METHOOD
 
-//3 Routers 
-const tourRouter=require('./routes/tourRouters')
-const userRouter=require('./routes/userRouters')
+//3 Routers
+const tourRouter = require("./routes/tourRouters");
+const userRouter = require("./routes/userRouters");
 
-app.use("/api/v1/tours",tourRouter)
-app.use("/api/v1/user",userRouter)
-
-
-const port = 300;
-
-//4 SERVER
-app.listen(port, () => {
-  console.log("appp runing");
-});
+app.use("/api/v1/tours", tourRouter);
+app.use("/api/v1/user", userRouter);
---
+++ Natours/server.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const dotenv=require('dotenv')
+const app = express();
+dotenv.config({path:'./config.env'})
+const port = 300;
+//4 SERVER
+app.listen(port, () => {
+    console.log("appp runing");
+  });
+
+  const x=23
+  x=50
