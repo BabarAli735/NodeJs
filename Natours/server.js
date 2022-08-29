f7dc842638acef96179aa2481566bab55dc03dfa
Babarali345 <babar@tecizeverything.com>
2022-08-29 11:39:23 +0500

    Connecting MongoDb Successfull

--- Natours/server.js
+++ Natours/server.js
@@ -1,11 +1,22 @@
 const express = require("express");
-const dotenv=require('dotenv')
+const dotenv = require("dotenv");
+const mongoose = require("mongoose");
 const app = express();
-dotenv.config({path:'./config.env'})
+dotenv.config({ path: "./config.env" });
+
+const DB = process.env.DATABASE.replace(
+  "<passwor>",
+  process.env.DATABASE_PASSWORD
+);
+mongoose.connect(DB, {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
+  useUnifiedTopology: true
+}).then(con=>console.log('DB Connecting Sccessfully '))
 const port = 300;
+
 //4 SERVER
 app.listen(port, () => {
-    console.log("appp runing");
-  });
-
- 
+  console.log("appp runing");
+});
